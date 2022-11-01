d3931c6b8df7935612fd5c547c8dad38df350db7
Chethan DB <chethan.balakrishna@werner.com>
2022-11-01 10:35:05 -0500

    [PATCH] to test versioning (#48)
    * added job name and merged the last publish step
    
    * test patch test
    
    * testing bump version type checks
    
    * test to see a syntax change would help
    
    * to see if env value is captured at step level
    
    * trial to check for vaiable value persistance
    
    * check if syntax works
    
    * more testing with new syntax and sharing env vars
    
    * more syntax change to test version script
    
    * syntax changes to version script
    
    * negative testing
    
    * syntax check
    
    * test conditions
    
    * addressed issue with having run and uses together
    
    * more conditional changes
    
    * cosmetic changes and comments
    
    * to test changes to pipeline

--- projects/angular-components-lib/src/lib/angular-components-lib.component.ts
+++ projects/angular-components-lib/src/lib/angular-components-lib.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'acl-AngularComponentsLib',
-  template: ` <p>angular-components-lib works!!!</p> `,
+  template: ` <p>angular-components-lib works! Pipeline Works!!</p> `,
   styles: [],
 })
 export class AngularComponentsLibComponent implements OnInit {
